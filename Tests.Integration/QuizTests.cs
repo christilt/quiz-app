9f431a3ee107574dcf35c9fc94b204ae11f4f2d0
Chris Tilt <tiltchris+gh@gmail.com>
2025-07-08 21:07:09 +0100

    Add tests

---
+++ Tests.Integration/QuizTests.cs
@@ -0,0 +1,32 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Integration.Setup;
+using Xunit;
+
+namespace Tests.Integration;
+
+public class QuizTests : IClassFixture<TestApplicationFactory>
+{
+    private readonly HttpClient _api;
+
+    public QuizTests(TestApplicationFactory factory)
+    {
+        _api = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task GetQuizes_ShouldReturnEmptyList_WhenThereAreNoQuizes()
+    {
+        var response = await _api.GetAsync($"api/quizes");
+
+        response.IsSuccessStatusCode.ShouldBeTrue();
+        var items = await response.Content.ReadFromJsonAsync<IEnumerable<object>>();
+        items.ShouldNotBeNull();
+        items.ShouldBeEmpty();
+    }
+}
---
+++ Tests.Integration/Setup/TestApplicationFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Integration.Setup;
+
+public class TestApplicationFactory : WebApplicationFactory<App.WebApi.Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+
+        });
+    }
+}
